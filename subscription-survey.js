a47240eebb2b1a9b93fad6bfdbcf5b3a661e6a07
mac user <macuser@MacBookPro.lan>
2025-05-08 05:34:46 +0100

    Fix: Update GitHub Actions workflow to handle missing survey files

---
+++ subscription-survey.js
@@ -0,0 +1,3 @@
+// This is an empty placeholder file to prevent deployment errors
+// The actual survey functionality is now in the React component
+console.log("This file is deprecated. Please use the React version at /subscription-survey");
